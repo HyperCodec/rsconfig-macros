cdc05cbf99c695017a33020e5ed7df0589093cc8
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2023-01-30 16:04:39 -0500

    add documentation

---
+++ README.md
@@ -0,0 +1,41 @@
+# RSCONFIG-macros
+[<img alt="github" href="https://img.shields.io/github/last-commit/hypercodec/rsconfig-macros" height="20">](https://github.com/hypercodec/rsconfig-macros)
+[<img alt="crates.io" href="https://img.shields.io/crates/d/rsconfig-macros" height="20">](https://crates.io/crates/rsconfig-macros)
+[<img alt="docs.rs" href="https://img.shields.io/docsrs/rsconfig-macros" height="20">](https://docs.rs/rsconfig-macros)
+
+Macro implementation for RSCONFIG
+
+## Examples
+### FileConfig
+```rust
+#[derive(Debug, FileConfig)]
+struct TestConfig {
+    test: bool
+}
+impl YamlConfig for TestConfig {
+    fn from_yaml(yaml: Vec<yaml_rust::Yaml>) -> Self {
+        Self { test: *&yaml[0]["test"].as_bool().unwrap() }
+    }
+    fn save_yaml(&self, path: &str) -> Result<()> {
+        let mut data = "test: ".to_string();
+        data.push_str(self.test.to_string().as_str());
+        fs::write(path, data).unwrap();
+
+        Ok(())
+    }
+}
+impl JsonConfig for TestConfig {
+    fn from_json(val: Value) -> Self {
+        Self { test: val["test"].as_bool().unwrap() }
+    }
+    fn save_json(&self, path: &str) -> io::Result<()> {
+        // convert to json pretty format and save
+        let mut m: HashMap<&str, Value> = HashMap::new();
+        m.insert("test", Value::from(self.test));
+        let data = serde_json::to_string_pretty(&m).unwrap();
+        fs::write(path, data).unwrap();
+     
+        Ok(())
+    }
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,51 @@
+#![warn(missing_docs)]
+
+//! Macro implementation for RSCONFIG
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::DeriveInput;
 
+/// Implements the default empty FileConfig if the required filetypes exist
+/// ### Example
+/// ```rust
+/// // FileConfig derive macro being used instead of adding `impl FileConfig for TestConfig {}` at the end
+/// #[derive(Debug, FileConfig)]
+/// struct TestConfig {
+///     test: bool
+/// }
+///
+/// impl YamlConfig for TestConfig {
+///     fn from_yaml(yaml: Vec<yaml_rust::Yaml>) -> Self {
+///         Self { test: *&yaml[0]["test"].as_bool().unwrap() }
+///    }
+///
+///     fn save_yaml(&self, path: &str) -> Result<()> {
+///         let mut data = "test: ".to_string();
+///         data.push_str(self.test.to_string().as_str());
+///
+///         fs::write(path, data).unwrap();
+///
+///         Ok(())
+///     }
+/// }
+///
+/// impl JsonConfig for TestConfig {
+///     fn from_json(val: Value) -> Self {
+///         Self { test: val["test"].as_bool().unwrap() }
+///     }
+///
+///     fn save_json(&self, path: &str) -> io::Result<()> {
+///         // convert to json pretty format and save
+///         let mut m: HashMap<&str, Value> = HashMap::new();
+///         m.insert("test", Value::from(self.test));
+///         let data = serde_json::to_string_pretty(&m).unwrap();
+///         fs::write(path, data).unwrap();
+///      
+///         Ok(())
+///     }
+/// }
+/// ```
 #[proc_macro_derive(FileConfig)]
 pub fn derive_config(input: TokenStream) -> TokenStream {
     let name = syn::parse_macro_input!(input as DeriveInput).ident;
